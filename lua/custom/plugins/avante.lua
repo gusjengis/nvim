53b556e336a543e07c803715b49de4834e7741d5
gusjengis <anthony.j.green@outlook.com>
2025-06-02 17:17:23 -0700

    got rid of annoying hints in avante

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -4,7 +4,9 @@ return {
     event = 'VeryLazy',
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-    opts = {},
+    opts = {
+      hints = { enabled = false },
+    },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = function()
       if vim.loop.os_uname().sysname == 'Windows_NT' then
