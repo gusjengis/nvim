2ccd7fb9b2e005fa2bce0ebff3aebee32a016a74
gusjengis <anthony.j.green@outlook.com>
2025-06-20 11:25:59 -0700

    updated keymap descriptions

--- lua/keymaps/misc.lua
+++ lua/keymaps/misc.lua
@@ -3,5 +3,5 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>') -- Clear highlights on searc
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }) -- Exit terminal mode
 
 -- Ctrl + S from every other program ever. Again, why tf do I have to hit Esc + : + w + Enter when I could just hit Ctrl + S? Ctrl + S isn't even used in insert mode, so fucking stupid.
-vim.api.nvim_set_keymap('i', '<C-s>', '<Esc><Cmd>w!<CR>a', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-s>', '<Cmd>w!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-s>', '<Esc><Cmd>w!<CR>a', { noremap = true, silent = true, desc = 'Save file' })
+vim.api.nvim_set_keymap('n', '<C-s>', '<Cmd>w!<CR>', { noremap = true, silent = true, desc = 'Save file' })
--- lua/plugins/keybind_hints.lua
+++ lua/plugins/keybind_hints.lua
@@ -46,7 +46,7 @@ return {
         { '<leader>s', group = '[S]earch' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>h', group = '[H]arpoon', mode = { 'n' } },
       },
     },
   },
