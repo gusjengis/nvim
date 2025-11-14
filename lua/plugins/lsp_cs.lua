05cd550a7ec2777a775f76006b0ced3891173629
gusjengis <anthony.j.green@outlook.com>
2025-11-14 14:43:51 -0800

    folding was being annoying, hopefully fixed roslyn

--- lua/plugins/lsp_cs.lua
+++ lua/plugins/lsp_cs.lua
@@ -2,19 +2,11 @@ return {
   {
     'seblyng/roslyn.nvim',
     ft = 'cs',
-    ---@module 'roslyn.config'
-    ---@type RoslynNvimConfig
     opts = {
-      -- filewatching = 'roslyn',
-
-      targets = {
-        {
-          name = 'Hundo',
-          solution = '/home/gusjengis/wkspaces/Hundo/Hundo.sln',
-        },
-      },
-      -- your configuration comes here; leave empty for default settings
-      -- NOTE: You must configure `cmd` in `config.cmd` unless you have installed via mason
+      -- Completely ignore Unity's .slnx files so roslyn only sees .sln
+      ignore_target = function(target)
+        return target:match '%.slnx$' ~= nil
+      end,
     },
   },
 }
