3ea9d9d216a571466aaf810795a1ddd424d64ccf
gusjengis <anthony.j.green@outlook.com>
2026-03-21 11:17:48 -0700

    Improved diffview, syntax highlighting with added lines

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,3 +1,29 @@
+local function soften_diff_add_highlights()
+  local ok, diff_add = pcall(vim.api.nvim_get_hl, 0, { name = 'DiffAdd', link = false })
+  if not ok or vim.tbl_isempty(diff_add) or not diff_add.bg then
+    return
+  end
+
+  vim.api.nvim_set_hl(0, 'DiffAdd', { bg = diff_add.bg })
+  vim.api.nvim_set_hl(0, 'DiffviewDiffAdd', { bg = diff_add.bg })
+end
+
 return {
-  { 'sindrets/diffview.nvim' },
+  {
+    'sindrets/diffview.nvim',
+    opts = {
+      enhanced_diff_hl = true,
+    },
+    config = function(_, opts)
+      require('diffview').setup(opts)
+
+      local group = vim.api.nvim_create_augroup('custom_diffview_highlights', { clear = true })
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        group = group,
+        callback = soften_diff_add_highlights,
+      })
+
+      soften_diff_add_highlights()
+    end,
+  },
 }
